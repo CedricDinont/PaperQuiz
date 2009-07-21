dc2678a78ed478c68bc53368395222f606a608a8
François Gaillard <francois.gaillard@isen-lille.fr>
2009-07-21 12:13:45 +0000

    contrôles fichiers

--- web/view_files_common.php
+++ web/view_files_common.php
@@ -8,6 +8,10 @@ function getEnd($str, $sub ){
 	return substr( $str, strlen( $str ) - strlen( $sub ) );
 }
 
+function getStrWithoutEnd($str, $sub ){
+	return substr( $str, 0 , strlen( $str ) - strlen( $sub ) );
+}
+
 function showDirFiles($dir) {
   global $quiz;
   $files = array();
@@ -21,8 +25,33 @@ function showDirFiles($dir) {
   }
   sort($files);
   foreach ($files as $nb => $file) {
-    showFile($short_dir, $file);
+   	showFile($short_dir, $file);
   }
+ }
+ 
+ function getDirFiles($dir){
+  global $quiz;
+  $files = array();
+  $i = 0;
+  $d = dir($dir);
+  $short_dir = str_replace($quiz->getDir(), "", $dir);
+  while (false !== ($entry = $d->read())) {
+    if ($entry[0] != '.') {
+      $files[$i++] = $entry;
+    }
+  }
+  $tab_fic = array();
+  sort($files);
+  foreach ($files as $nb => $file) {
+  		$fic = getFile($short_dir, $file);
+  		$keys = array_keys($fic);
+  		$key = $keys[0];
+  		if(!array_key_exists($key,$tab_fic)){
+  			$tab_fic[$key] = array();
+  		}
+  		array_push($tab_fic[$key],$fic[$key]);
+  }
+  return $tab_fic;
  }
 
 function showFile($dir, $file) {
@@ -31,33 +60,79 @@ function showFile($dir, $file) {
     } else {
       $page = "view_file";
     }
+    
+    $types=array("data",".txt",".students",".csv",".marking",".corners",".answers",".log");
+    $types_fic = array(".jpg",".jpg.mmr_data",".jpg.omr1_data",".jpg.omr2_data",".jpg.omr3_data",".jpg_binarized.jpg",".jpg_corrected.jpg"
+    ,".jpg_corrected2.jpg",".jpg_corrected3.jpg");
+    
     $type = "";
-    if(endsWith($file, "data")){
-    	$type = "data";
+    foreach($types as $test)
+    {
+    	if(endsWith($file, $test)){
+    		$type = $test;
+    	}
     }
-    if(endsWith($file, ".txt")){
-    	$type = "txt";
+    
+    $nom_fic = "";
+    foreach($types_fic as $test_fic)
+    {
+    	if(strpos($file,$test_fic) >0){
+    		$nom_fic = getStrWithoutEnd($file,$test_fic);
+    	}
     }
-    if(endsWith($file, ".students")){
-    	$type = "students";
+    
+    echo "<li><input ";
+    if($type != ""){
+    	echo "type_fichier=\"".$type."\" ";
     }
-    if(endsWith($file, ".csv")){
-    	$type = "csv";
+    if($nom_fic != ""){
+    	echo "fichier=\"".$nom_fic."\" ";
     }
-    if(endsWith($file, ".marking")){
-    	$type = "marking";
-    }
-    if(endsWith($file, ".corners")){
-    	$type = "corners";
-    }
-    if(endsWith($file, ".answers")){
-    	$type = "answers";
-    }
-    if(endsWith($file, ".log")){
-    	$type = "log";
-    }
-    echo "<li><input type_fichier=".$type." type=\"checkbox\" name=\"files[]\" value=\"".$dir.$file."\"><a href=\"".$page.".php?quiz-id=".$_GET['quiz-id']."&filename=".$dir.$file."\">".$file."</a></li>\n";
+    echo "type=\"checkbox\" name=\"files[]\" value=\"".$dir.$file."\"><a href=\"".$page.".php?quiz-id=".$_GET['quiz-id']."&filename=".$dir.$file."\">".$file."</a></li>\n";
 }
+
+function getFile($dir, $file) {
+    if (endsWith($file, "data") || endsWith($file, ".txt") || endsWith($file, "students") || endsWith($file, ".csv") || endsWith($file, ".marking") || endsWith($file, "corners") || endsWith($file, "answers") || endsWith($file, ".log")) {
+      $page = "view_text_file_content";
+    } else {
+      $page = "view_file";
+    }
+    
+    $types=array(".jpg",".jpg.omr1_data",".jpg.omr2_data",".jpg.omr3_data",".jpg_binarized.jpg",".jpg_corrected.jpg"
+    ,".jpg_corrected2.jpg",".jpg_corrected3.jpg",".txt",".students",".csv",".marking",".corners",".answers",".log");
+    $types_fic = array(".jpg",".jpg.mmr_data",".jpg.omr1_data",".jpg.omr2_data",".jpg.omr3_data",".jpg_binarized.jpg",".jpg_corrected.jpg"
+    ,".jpg_corrected2.jpg",".jpg_corrected3.jpg");
+    
+    $type = "";
+    foreach($types as $test)
+    {
+    	if(endsWith($file, $test)){
+    		$type = $test;
+    	}
+    }
+    
+    $nom_fic = "";
+    foreach($types_fic as $test_fic)
+    {
+    	if(strpos($file,$test_fic) >0){
+    		$nom_fic = getStrWithoutEnd($file,$test_fic);
+    	}
+    }
+    return array($nom_fic=>array("file"=>$file,"chemin"=>$dir.$file,"page"=>$page,"type_fichier"=>$type,"fichier"=>$nom_fic,"quiz-id"=>$_GET['quiz-id']));
+}
+
+function showFileFromDescription($tab_file){
+	 echo "<li><input ";
+    if($tab_file["type_fichier"] != ""){
+    	echo "type_fichier=\"".$tab_file["type_fichier"]."\" ";
+    }
+    if($tab_file["fichier"] != ""){
+    	echo "fichier=\"".$tab_file["fichier"]."\" ";
+    }
+    echo "type=\"checkbox\" name=\"files[]\" value=\"".$tab_file["chemin"]."\"><a href=\"".$tab_file["page"].
+    ".php?quiz-id=".$tab_file["quiz-id"]."&filename=".$tab_file["chemin"]."\">".$tab_file["file"]."</a></li>\n";
+}
+
 
 function doFilesMenu() {
   echo "<script>function confirmAction() { if (confirm(\"Voulez-vous vraiment réaliser cette opération ?\")) { document.file_operations_form.submit(); } }  </script>";
--- web/view_omr_files.php
+++ web/view_omr_files.php
@@ -1,39 +1,112 @@
 <?php
 	require_once('Quiz.class.php');
 	require_once('quiz_common.php');
-        require_once('view_files_common.php');
-
+   require_once('view_files_common.php');
+	
 	$quiz = Quiz::getQuizById($_GET['quiz-id']);
         doFileOperation();
 ?>
 <html>
 <head>
 	<title>Visualisation des fichiers liés à la reconnaissance des marques</title>
-        <link rel="stylesheet" type="text/css" href="style/quiz.css" />
+   <link rel="stylesheet" type="text/css" href="style/quiz.css" />
+   <script type="text/javascript" src="./js/fichier.js"></script>
 </head>
 <body>
 
 <form method="POST" action="view_omr_files.php?quiz-id=<?php echo $quiz->getId(); ?>" name="file_operations_form">
 
+<div>
 <h1>Fichiers du répertoire d'entrée</h1>
+<?php
+include("files_contol.php.inc");
+?>
 <ul>
-<?php showDirFiles($quiz->getOmrInputDir()) ?>
+<?php
+$tab_files = getDirFiles($quiz->getOmrInputDir());
+foreach($tab_files as $group){
+	?>
+	<li>
+	<input type="button" onClick="invertAllinSection(this.parentNode);" value="tous"></input>
+	<ul>
+	<?php
+	foreach($group as $tab_file){
+		if($tab_file != null && $tab_file["file"] != null){
+			showFileFromDescription($tab_file);
+		}
+	}
+	?>
+	</ul>
+	</li>
+	<?php
+}
+?>
 </ul>
+</div>
 
+<div>
 <h1>Fichiers du répertoire de sortie</h1>
+<?php
+include("files_contol.php.inc");
+?>
 <ul>
-<?php showDirFiles($quiz->getOmrOutputDir()) ?>
+<?php
+$tab_files = getDirFiles($quiz->getOmrOutputDir());
+foreach($tab_files as $group){
+	?>
+	<li>
+	<input type="button" onClick="invertAllinSection(this.parentNode);" value="tous"></input>
+	<ul>
+	<?php
+	foreach($group as $tab_file){
+		if($tab_file != null && $tab_file["file"] != null){
+			showFileFromDescription($tab_file);
+		}
+	}
+	?>
+	</ul>
+	</li>
+	<?php
+}
+?>
 </ul>
+</div>
 
+<div>
 <h1>Fichiers du répertoire d'erreur</h1>
+<?php
+include("files_contol.php.inc");
+?>
 <ul>
-<?php showDirFiles($quiz->getOmrErrorDir()) ?>
+<?php
+$tab_files = getDirFiles($quiz->getOmrErrorDir());
+foreach($tab_files as $group){
+	?>
+	<li>
+	<input type="button" onClick="invertAllinSection(this.parentNode);" value="tous"></input>
+	<ul>
+	<?php
+	foreach($group as $tab_file){
+		if($tab_file != null && $tab_file["file"] != null){
+			showFileFromDescription($tab_file);
+		}
+	}
+	?>
+	</ul>
+	</li>
+	<?php
+}
+?>
 </ul>
+</div>
 
+<div>
 <h1>Autres fichiers</h1>
 <ul>
 <?php showFile("./", "omr.log"); ?>
 </ul>
+</div>
+
 <br><br>
 
 <?php doFilesMenu(); ?>
