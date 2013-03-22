4cacdbcb5be8bbc75c8ce5791cb18d37cda67479
Olivier Rossiny <olivier.rossiny@isen-lille.fr>
2013-03-22 17:17:07 +0000

    Comme d'hab, j'oublie d'ajouter les fichiers de bootstrap

---
+++ web/end_header.php
@@ -0,0 +1,17 @@
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="navbar-inner">
+	  <ul class="nav">
+	    <li class=""><a href="index.php"><i class="icon-home"></i> QuizApp</a></li>
+	    <li><a href="create_quiz.php"><i class="icon-new"></i> New</a></li>
+	  </ul>
+	  <ul class="nav pull-right">
+	    <li><a href="help.php"><i class="icon-question-sign"></i> Aide</a></li>
+	  </ul>
+      </div>
+    </div>
+    
+    <!-- content -->
+    <div class="container">
+      <h2><?php echo $page_title ?></h2> 
