df4e516edf52ee191f8b9dd67378cafe9f9aa7b9
Olivier Rossiny <olivier.rossiny@isen-lille.fr>
2013-03-23 13:52:10 +0000

    Amélioration du formulaire de création

--- web/create_quiz.php
+++ web/create_quiz.php
@@ -21,12 +21,13 @@ Veuillez renplir le formulaire suivant en respectant les règles suivantes :
 <li>Le nom du groupe d'étudiants doit commencer par le nom de la promo suivi éventuellement du sous-groupe concerné séparés par un _ (ex.: CIR1, ou M1_Unix).</li>
 <li>L'intitulé doit permettre de reconnaître le sujet du quiz (ex. : TP1, DS).</li>
 </ul>
-<form action="create_quiz.php" method="post">
+<form action="create_quiz.php" class="form-horizontal" method="post">
 <input type="hidden" name="action" value="create">
-<table>
-<tr><td>Jour :</td><td> 
-	<select name="day" class="form_elem">
-	<?php
+  <div class="control-group">
+    <label class="control-label" for="day">Jour&nbsp;:</label>
+    <div class="controls">
+      <select name="day" class="form_elem">
+	  <?php
 		for($i=1;$i<=31;$i++)
 		{
 			echo "<option";
@@ -46,9 +47,13 @@ Veuillez renplir le formulaire suivant en respectant les règles suivantes :
 			echo "</option>\n";
 		}
 	?>
-	</select></td>
-	</tr><tr><td>Mois : </td><td> 
-	<select name="month" class="form_elem">
+	</select>
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="month">Mois&nbsp;:</label>
+    <div class="controls">
+      <select name="month" class="form_elem">
 	<?php
 		$tab_mois = array(1=>"Janvier",2=>"Février",3=>"Mars",4=>"Avril",5=>"Mai",6=>"Juin",7=>"Juillet",
 		8=>"Août",9=>"Septembre",10=>"Octobre",11=>"Novembre",12=>"Décembre");
@@ -72,9 +77,13 @@ Veuillez renplir le formulaire suivant en respectant les règles suivantes :
 			echo ">".$tab_mois[$i]."</option>\n";
 		}
 	?>
-        </select></td>
-       </tr><tr><td>Année : </td><td> 
-	<select name="year" class="form_elem">
+        </select>
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="year">Année&nbsp;:</label>
+    <div class="controls">
+      <select name="year" class="form_elem">
 	<?php
 		for($i=$annee-1;$i<=$annee+2;$i++)
 		{
@@ -97,11 +106,26 @@ Veuillez renplir le formulaire suivant en respectant les règles suivantes :
 			echo ">".($i)."</option>\n";
 		}
 	?>
-	</select></td>
-</tr><tr><td>Groupe d'étudiants :</td><td> <input type="text" name="group" class="form_elem"></td>
-</tr><tr><td>Intitulé du quiz : </td><td><input type="text" name="quiz-name" class="form_elem"></td>
-</table>
-	<button type="submit" class="btn btn-primary">Créer le quiz</button>
+	</select>
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="group">Groupe d'étudiants&nbsp;:</label>
+    <div class="controls">
+      <input type="text" name="group" class="form_elem">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="quiz-name">Intitulé du quiz&nbsp;:</label>
+    <div class="controls">
+      <input type="text" name="quiz-name" class="form_elem">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn btn-primary">Créer le quiz</button>
+    </div>
+  </div>
 </form>
 <?php }
   doMainMenu() ?>
